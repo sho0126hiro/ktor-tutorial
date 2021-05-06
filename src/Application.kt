9be77b042a048a5b21671084692533a814d52035
Sho Hirose <sho0126hiro@gmail.com>
2021-05-07 00:48:42 +0900

    503

---
+++ src/Application.kt
@@ -0,0 +1,22 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+}
+
